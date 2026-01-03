24f959a3d6b780581b514dd2578590e591324578
Subhan Zaheer <xheksubhan.zaheer@gmail.com>
2026-01-04 03:16:01 +0500

    Add files via upload

---
+++ JS Intro/script.js
@@ -0,0 +1,19 @@
+  alert("Hello World")
+
+ 
+   var a = prompt("Enter your number")
+   var isTrue = confirm("Are you sure you want to leave this page and Blast your computer")
+
+   if(isTrue){
+    console.log("Computer is blasting")
+   }
+
+   else{
+     console.log("Computer is not blasting")
+   }
+
+   console.log("Your number is " + a)
+
+   document.title = "Hey i am good"
+
+   document.body.style.backgroundColor = "red"
